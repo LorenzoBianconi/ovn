LorenzoBianconi/ovn

commit f49b17a6cbe30ee05dc9743554c8ece92b16db48

OVN: use trigger_event action to report 'empty_lb_rule' events
Add northd logical flows in order to reports that the controller
received an IP packet for LB rule witn no backends.
This configuration is used by OpenShift to spin up a idle POD

Signed-off-by: Mark Michelson <mmichels@redhat.com>
Co-authored-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Ben Pfaff <blp@ovn.org>

northd/ovn-northd.c
@@ -70,6 +70,8 @@ static const char *unixctl_path;
 static struct hmap macam = HMAP_INITIALIZER(&macam);
 static struct eth_addr mac_prefix;
 
+static bool controller_event_en;
+
 #define MAX_OVN_TAGS 4096
 
 /* Pipeline stages. */
@@ -3624,6 +3626,34 @@ build_pre_lb(struct ovn_datapath *od, struct hmap *lflows)
 
             if (!sset_contains(&all_ips, ip_address)) {
                 sset_add(&all_ips, ip_address);
+            }
+
+            if (controller_event_en && !node->value[0]) {
+                struct ds match = DS_EMPTY_INITIALIZER;
+                char *action;
+
+                if (addr_family == AF_INET) {
+                    ds_put_format(&match, "ip4.dst == %s && %s",
+                                  ip_address, lb->protocol);
+                } else {
+                    ds_put_format(&match, "ip6.dst == %s && %s",
+                                  ip_address, lb->protocol);
+                }
+                if (port) {
+                    ds_put_format(&match, " && %s.dst == %u", lb->protocol,
+                                  port);
+                }
+                action = xasprintf("trigger_event(event = \"%s\", "
+                               "vip = \"%s\", protocol = \"%s\", "
+                               "load_balancer = \"" UUID_FMT "\");",
+                               event_to_string(OVN_EVENT_EMPTY_LB_BACKENDS),
+                               node->key, lb->protocol,
+                               UUID_ARGS(&lb->header_.uuid));
+                ovn_lflow_add(lflows, od, S_SWITCH_IN_PRE_LB, 120,
+                              ds_cstr(&match), action);
+                ds_destroy(&match);
+                free(action);
+                continue;
             }
 
             free(ip_address);
@@ -8114,6 +8144,9 @@ ovnnb_db_run(struct northd_context *ctx,
 
         smap_destroy(&options);
     }
+
+    controller_event_en = smap_get_bool(&nb->options,
+                                        "controller_event", false);
 
     cleanup_macam(&macam);
 }

ovn-nb.xml
@@ -107,6 +107,17 @@
         Configure a given OUI to be used as prefix when L2 address is
         dynamically assigned, e.g. <code>00:11:22</code>
       </column>
+
+      <column name="options" key="controller_event" type='{"type": "boolean"}'>
+        Value set by the CMS to enable/disable ovn-controller event reporting.
+        Traffic into OVS can raise a 'controller' event that results in a
+        Controller_Event being written to the <ref table="Controller_Event"/>
+        table in SBDB. When the CMS has seen the event and taken appropriate
+        action, it can remove the correponding row in
+        <ref table="Controller_Event"/> table.
+        The intention is for a CMS to see the events and take some sort of
+        action. Please see the <ref table="Controller_Event"/> table in SBDB.
+      </column>
     </group>
 
     <group title="Connection Options">

commit 6b192682f88c91d550dd3181254987a33a6c036b

Fix memory leak in build_pre_lb
Fix memory leak of ip_address string in build_pre_lb routine if we
install logical flows for empty_lb controller event

Fixes: f49b17a6cbe3 ("OVN: use trigger_event action to report 'empty_lb_rule' events")

northd/ovn-northd.c
@@ -4063,7 +4063,6 @@ build_pre_lb(struct ovn_datapath *od, struct hmap *lflows)
                               ds_cstr(&match), action);
                 ds_destroy(&match);
                 free(action);
-                continue;
             }
 
             free(ip_address);