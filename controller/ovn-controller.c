ce1dab4e1caa801028e1445c0054cc9d52691526
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2021-11-11 22:27:24 +0100

    controller: reconfigure ovs meters for ovn meters updates
    At the moment ovs meters are reconfigured by ovn just when a
    new a meter is allocated while updates for an already allocated meter are
    ignored. This issue can be easily verified with the following reproducer:
    
    $ovn-nbctl meter-add meter0 drop 10 pktps
    $ovn-nbctl --log --meter=meter0 acl-add sw0 to-lport 1000 'tcp.dst == 80' drop
    $ovn-nbctl --may-exist meter-add meter0 drop 20 pktps
    $ovs-ofctl -O OpenFlow15 dump-meters br-int
    
    Fix the issue reconfiguring ovs meters even for ovn meters updates.
    Related bz: https://bugzilla.redhat.com/show_bug.cgi?id=1939524

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -76,6 +76,7 @@
 #include "stopwatch.h"
 #include "lib/inc-proc-eng.h"
 #include "hmapx.h"
+#include "openvswitch/ofp-util.h"
 
 VLOG_DEFINE_THIS_MODULE(main);
 
@@ -114,6 +115,7 @@ static unixctl_cb_func debug_delay_nb_cfg_report;
 #define OVS_STARTUP_TS_NAME "ovn-startup-ts"
 
 static struct engine *flow_engine;
+static struct engine *meter_engine;
 
 static char *parse_options(int argc, char *argv[]);
 OVS_NO_RETURN static void usage(void);
@@ -963,7 +965,8 @@ ctrl_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     SB_NODE(dhcpv6_options, "dhcpv6_options") \
     SB_NODE(dns, "dns") \
     SB_NODE(load_balancer, "load_balancer") \
-    SB_NODE(fdb, "fdb")
+    SB_NODE(fdb, "fdb") \
+    SB_NODE(meter, "meter")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -1505,6 +1508,55 @@ addr_sets_sb_address_set_handler(struct engine_node *node, void *data)
     }
 
     as->change_tracked = true;
+    return true;
+}
+
+struct ed_type_meter {
+    bool change_tracked;
+};
+
+static void *
+en_meter_init(struct engine_node *node OVS_UNUSED,
+              struct engine_arg *arg OVS_UNUSED)
+{
+    struct ed_type_meter *m = xzalloc(sizeof *m);
+
+    m->change_tracked = false;
+    return m;
+}
+
+static void
+en_meter_cleanup(void *data OVS_UNUSED)
+{
+}
+
+static void
+en_meter_run(struct engine_node *node, void *data)
+{
+    struct ed_type_meter *m = data;
+
+    engine_set_node_state(node, EN_UPDATED);
+    m->change_tracked = false;
+}
+
+static bool
+meter_sb_meter_handler(struct engine_node *node, void *data)
+{
+    struct ovs_list msgs = OVS_LIST_INITIALIZER(&msgs);
+    struct ed_type_meter *m = data;
+
+    struct sbrec_meter_table *m_table =
+        (struct sbrec_meter_table *)EN_OVSDB_GET(
+            engine_get_input("SB_meter", node));
+
+    const struct sbrec_meter *iter;
+    SBREC_METER_TABLE_FOR_EACH_TRACKED (iter, m_table) {
+        if (!sbrec_meter_is_deleted(iter) && !sbrec_meter_is_new(iter)) {
+            update_meter(iter);
+        }
+    }
+    m->change_tracked = true;
+
     return true;
 }
 
@@ -3220,6 +3272,7 @@ main(int argc, char *argv[])
     ENGINE_NODE(lflow_output, "logical_flow_output");
     ENGINE_NODE(flow_output, "flow_output");
     ENGINE_NODE(addr_sets, "addr_sets");
+    ENGINE_NODE(meter, "meter");
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA(port_groups, "port_groups");
 
 #define SB_NODE(NAME, NAME_STR) ENGINE_NODE_SB(NAME, NAME_STR);
@@ -3344,11 +3397,14 @@ main(int argc, char *argv[])
     engine_add_input(&en_flow_output, &en_pflow_output,
                      flow_output_pflow_output_handler);
 
+    engine_add_input(&en_meter, &en_sb_meter, meter_sb_meter_handler);
+
     struct engine_arg engine_arg = {
         .sb_idl = ovnsb_idl_loop.idl,
         .ovs_idl = ovs_idl_loop.idl,
     };
     engine_init(&flow_engine, &en_flow_output, &engine_arg);
+    engine_init(&meter_engine, &en_meter, &engine_arg);
 
     engine_ovsdb_node_add_index(&en_sb_chassis, "name", sbrec_chassis_by_name);
     engine_ovsdb_node_add_index(&en_sb_multicast_group, "name_datapath",
@@ -3481,6 +3537,7 @@ main(int argc, char *argv[])
         }
 
         engine_init_run(flow_engine);
+        engine_init_run(meter_engine);
 
         struct ovsdb_idl_txn *ovs_idl_txn = ovsdb_idl_loop_run(&ovs_idl_loop);
         unsigned int new_ovs_cond_seqno
@@ -3604,6 +3661,7 @@ main(int argc, char *argv[])
 
                     stopwatch_start(CONTROLLER_LOOP_STOPWATCH_NAME,
                                     time_msec());
+                    engine_run(meter_engine, true);
                     if (ovnsb_idl_txn) {
                         if (!ofctrl_can_put()) {
                             /* When there are in-flight messages pending to
@@ -3898,6 +3956,7 @@ loop_done:
 
     engine_set_context(flow_engine, NULL);
     engine_cleanup(&flow_engine);
+    engine_cleanup(&meter_engine);
 
     /* It's time to exit.  Clean up the databases if we are not restarting */
     if (!restart) {
